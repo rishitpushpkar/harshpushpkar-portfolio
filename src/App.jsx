f31a579d8246d239c38b84dea4f391e233464bd0
Rishit Pushpkar <rishit.02pushpkar@gmail.com>
2024-05-28 00:29:33 +0530

    router basename

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import ScrollToTop from "./utils/ScrollToTop";
 function App() {
   return (
     <>
-      <BrowserRouter basename="/">
+      <BrowserRouter basename="/Portfolio-Website">
         <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
