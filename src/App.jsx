119abb21aa9fc3130929442d0d8b823159bd99fd
Rishit Pushpkar <rishit.02pushpkar@gmail.com>
2024-05-29 16:56:53 +0530

    changes for hostinger : basename(removed) and base (/)

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import ScrollToTop from "./utils/ScrollToTop";
 function App() {
   return (
     <>
-      <BrowserRouter basename="/harshpushpkar-portfolio">
+      <BrowserRouter>
         <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
